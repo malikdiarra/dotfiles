e0612246f74903d1391738363b39701bdf5c4bed
Malik Diarra <malik.diarra@gmail.com>
2014-04-27 22:35:17 +0200

    adding force option for deployment

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -2,9 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+flag=""
+if [ $1 == "force" ]
+then
+  flag="--force"
+fi
+
 function deploy_file()
 {
-  ln -s $DIR/$1 ~/.$1
+  ln $flag -s $DIR/$1 ~/.$1
 }
 
 for i in vimrc vim gitignore gitconfig githelpers bashrc profile

e5812fa96251ac156818681919a8dbfc8766d177
Malik Diarra <malik.diarra@gmail.com>
2018-10-22 21:49:05 -0700

    Use stow for deployment
    Stow packages for:
    * stow
    * git
    * bash
    * i3
    * XResource

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -1,19 +1,9 @@
 #! /bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-flag=""
-if [ $1 == "force" ]
-then
-  flag="--force"
-fi
-
-function deploy_file()
-{
-  ln $flag -s $DIR/$1 ~/.$1
-}
-
-for i in vimrc vim gitignore gitconfig githelpers bashrc profile
-do
-  deploy_file $i
-done
+stow stow
+stow vim
+stow bash
+stow bin
+stow Xresources
+stow git
+stow i3

fef70bc1d3af29cae7c77bc7ff2f23fc7b1d61b6
Malik Diarra <malik@airbyte.io>
2023-12-30 21:11:25 +0100

    [VIM] Add keybindings for lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -3,9 +3,37 @@ require("mason-lspconfig").setup({ ensure_installed = {"gopls", "jdtls"} })
 vim.lsp.set_log_level('debug')
 local util = require("lspconfig/util")
 
+local on_attach = function(client, bufnr)
+  vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true, buffer = bufnr})
+  vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", {noremap = true, silent = true, buffer = bufnr})
+
+  vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true, buffer = bufnr})
+  vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true, buffer = bufnr})
+
+  vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true, silent = true, buffer = bufnr})
+  vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true, buffer = bufnr})
+  vim.keymap.set("n", "<leader>ds", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", {noremap = true, silent = true, buffer = bufnr})
+
+
+  vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", {noremap = true, silent = true, buffer = bufnr})
+
+  vim.keymap.set("n", "<leader>td", "<cmd>lua vim.lsp.buf.type_definition()<CR>", {noremap = true, silent = true, buffer = bufnr})
+
+  vim.keymap.set("n", "<leader>ic", "<cmd>lua vim.lsp.buf.incoming_calls()<CR>", {noremap = true, silent = true, buffer = bufnr})
+  vim.keymap.set("n", "<leader>oc", "<cmd>lua vim.lsp.buf.outgoing_calls()<CR>", {noremap = true, silent = true, buffer = bufnr})
+
+  vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", {noremap = true, silent = true, buffer = bufnr})
+  vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", {noremap = true, silent = true, buffer = bufnr})
+  vim.keymap.set("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", {noremap = true, silent = true, buffer = bufnr})
+  vim.keymap.set("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", {noremap = true, silent = true, buffer = bufnr})
+
+  vim.keymap.set("n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", {noremap = true, silent = true, buffer = bufnr})
+end
+
+vim.lsp.set_log_level('debug')
+
 require("lspconfig").gopls.setup {
   on_attach = on_attach,
-  capabilities = capabilities,
   cmd = {"gopls"},
   filetypes = {"go", "gomod", "gowork", "gotmpl"},
   root_dir = util.root_pattern("go.work", "go.mod", ".git"),

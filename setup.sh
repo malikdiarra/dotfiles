110795896ecdff0fdef1de48e5636417e7a37399
Malik Diarra <malik.diarra@gmail.com>
2025-02-14 15:36:10 -0800

    chore: merge deploy.sh and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
-cd ~
-[ ! -d .dotfiles ] && git clone https://github.com/malikdiarra/dotfiles.git .dotfiles
-[ -f .bashrc ] && mv .bashrc .bashrc.old
-[ -f .profile ] && mv .profile .profile.old
-cd .dotfiles
-git submodule update --init
-./deploy_dotfiles.sh
-cd ..
-source .bashrc
+#! /bin/bash
+
+if command -v stow 2>&1 >/dev/null
+then
+  echo "Deploying dotfiles"
+  stow --no-folding stow
+  stow --no-folding vim
+  stow --no-folding nvim
+  stow --no-folding bash
+  stow --no-folding Xresources
+  stow --no-folding git
+  stow --no-folding i3
+  stow --no-folding tmux
+else
+  echo "Stow is not available, Skipping deployment"
+fi

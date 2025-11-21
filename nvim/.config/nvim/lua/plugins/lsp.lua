589f5b7dbb15d9c593ca43bee6a207bbc9465d5b
Malik Diarra <malik.diarra@gmail.com>
2025-11-21 12:41:54 -0800

    [VIM] add vtsls lsp configuration

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -82,3 +82,8 @@ require("lspconfig").jedi_language_server.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
+
+require("lspconfig").vtsls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}

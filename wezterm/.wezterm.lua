802ec3a1cd439e67f3525de57822c634f843e414
Malik Diarra <malik.diarra@gmail.com>
2026-06-25 22:05:19 -0700

    feat(wezterm): first version of wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require 'wezterm'
+
+return {
+  -- basic defaults
+  color_scheme = "Catppuccin Mocha",
+  font_size = 14.0,
+
+  -- important for your use case
+  enable_tab_bar = true,
+  use_fancy_tab_bar = true,
+  keys = {
+    {
+      key = 'g',
+      mods = 'CMD',
+      action = wezterm.action.SplitPane {
+        direction = 'Right',
+        size = { Percent = 30 },
+        command = {
+          args = {
+            'bash',
+            '-lc',
+            'watch -t -c -n 2 "git -c color.status=always status -sb"'
+          },
+        },
+      },
+    },
+  },
+}
